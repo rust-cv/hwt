99a07d3229fdcf592f776480b28d5674adda802a
Geordon Worley <vadixidav@gmail.com>
2019-03-03 11:34:09 -0500

    make search_radius_2 benchmark parameterized

--- benches/neighbors.rs
+++ benches/neighbors.rs
@@ -4,18 +4,22 @@ use hwt::*;
 fn bench_neighbors(c: &mut Criterion) {
     c.bench(
         "neighbors",
-        Benchmark::new("search_2^12_neighbors_radius_2", |bencher: &mut Bencher| {
-            let range = (0..).take(1 << 12);
-            let mut hwt = Hwt::new();
-            for i in range.clone() {
-                hwt.insert(u128::from(i), i, u128::from);
-            }
-            let mut cycle_range = range.cycle();
-            bencher.iter(|| {
-                let feature = cycle_range.next().unwrap();
-                assert!(hwt.neighbors(2, u128::from(feature), &u128::from).count() < 8128);
-            });
-        }),
+        ParameterizedBenchmark::new(
+            "search_radius_2",
+            |bencher: &mut Bencher, &total: &usize| {
+                let range = (0..).take(total);
+                let mut hwt = Hwt::new();
+                for i in range.clone() {
+                    hwt.insert(u128::from(i), i, u128::from);
+                }
+                let mut cycle_range = range.cycle();
+                bencher.iter(|| {
+                    let feature = cycle_range.next().unwrap();
+                    assert!(hwt.neighbors(2, u128::from(feature), &u128::from).count() < 8128);
+                });
+            },
+            (1..6).map(|n| 10usize.pow(n)),
+        ),
     );
 }
 
